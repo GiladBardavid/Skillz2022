54a8b1c57f9f8b0fee6c49647710d2ee9fda17a8
GiladBardavid <gilad@bardavid.com>
2022-02-15 11:45:02 +0200

    Added initial project

---
+++ src/bots/Log.java
@@ -0,0 +1,17 @@
+package bots;
+
+public class Log {
+
+    // A boolean that we set as true if we want to print out debug strings, and false if not
+    public static boolean IS_DEBUG = true;
+
+    /**
+     * A function to print strings if we are in debug mode
+     * @param toPrint The string to print
+     */
+    public static void log(Object toPrint) {
+        if(IS_DEBUG) {
+            System.out.println(toPrint);
+        }
+    }
+}
