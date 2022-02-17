26edf3142a6b4286f9b713ac23e1aad52510955f
GiladBardavid <gilad@bardavid.com>
2022-02-17 15:31:57 +0200

    Added neededhelp class

---
+++ src/bots/NeededHelp.java
@@ -0,0 +1,26 @@
+package bots;
+
+import penguin_game.*;
+import java.util.*;
+
+public class NeededHelp {
+
+    public int howManyPenguins;
+    public int inHowManyTurns;
+
+    public NeededHelp(int howManyPenguins, int inHowManyTurns) {
+        this.howManyPenguins = howManyPenguins;
+        this.inHowManyTurns = inHowManyTurns;
+    }
+
+
+
+    public static NeededHelp getNeededHelp(Game game, IceBuilding myIceBuilding) {
+        for(int i = 0; i < game.maxTurns; i++) {
+            int penguinAmountInXTurns = GameUtil.getPenguinAmountInTurnXForEnemyOrNeutralIceBuilding(game, myIceBuilding, i);
+            if(penguinAmountInXTurns <= 0) {
+                return new NeededHelp(-penguinAmountInXTurns + 1, i);
+            }
+        }
+    }
+}
